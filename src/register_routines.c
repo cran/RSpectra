311536ab15734ab7fe97a837325fc227ba51f3bf
Yixuan Qiu <yixuan.qiu@cos.name>
2016-02-29 11:17:36 +0000

    version 0.11-0

---
+++ src/register_routines.c
@@ -0,0 +1,13 @@
+#include <SpectraC.h>
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+void R_init_RSpectra(DllInfo *info)
+{
+    R_RegisterCCallable("RSpectra", "eigs_sym_c",       (DL_FUNC) eigs_sym_c);
+    R_RegisterCCallable("RSpectra", "eigs_sym_shift_c", (DL_FUNC) eigs_sym_shift_c);
+    R_RegisterCCallable("RSpectra", "eigs_gen_c",       (DL_FUNC) eigs_gen_c);
+    R_RegisterCCallable("RSpectra", "eigs_gen_shift_c", (DL_FUNC) eigs_gen_shift_c);
+}
+
