698e7897569598f8d681a5f6cda97a73c1f891ab
Yixuan Qiu <yixuan.qiu@cos.name>
2018-05-22 21:37:53 +0000

    version 0.13-1

--- inst/include/Spectra/MatOp/SparseSymMatProd.h
+++ inst/include/Spectra/MatOp/SparseSymMatProd.h
@@ -4,10 +4,11 @@
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#ifndef DENSE_SYM_MAT_PROD_H
-#define DENSE_SYM_MAT_PROD_H
+#ifndef SPARSE_SYM_MAT_PROD_H
+#define SPARSE_SYM_MAT_PROD_H
 
 #include <Eigen/Core>
+#include <Eigen/SparseCore>
 
 namespace Spectra {
 
@@ -16,34 +17,29 @@ namespace Spectra {
 /// \ingroup MatOp
 ///
 /// This class defines the matrix-vector multiplication operation on a
-/// symmetric real matrix \f$A\f$, i.e., calculating \f$y=Ax\f$ for any vector
+/// sparse real symmetric matrix \f$A\f$, i.e., calculating \f$y=Ax\f$ for any vector
 /// \f$x\f$. It is mainly used in the SymEigsSolver eigen solver.
 ///
-template <typename Scalar, int Uplo = Eigen::Lower>
-class DenseSymMatProd
+template <typename Scalar, int Uplo = Eigen::Lower, int Flags = 0, typename StorageIndex = int>
+class SparseSymMatProd
 {
 private:
-    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
     typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
-    typedef Eigen::Map<const Matrix> MapConstMat;
     typedef Eigen::Map<const Vector> MapConstVec;
     typedef Eigen::Map<Vector> MapVec;
+    typedef Eigen::SparseMatrix<Scalar, Flags, StorageIndex> SparseMatrix;
 
-    typedef const Eigen::Ref<const Matrix> ConstGenericMatrix;
-
-    const MapConstMat m_mat;
+    const SparseMatrix& m_mat;
 
 public:
     ///
     /// Constructor to create the matrix operation object.
     ///
-    /// \param mat_ An **Eigen** matrix object, whose type can be
-    /// `Eigen::Matrix<Scalar, ...>` (e.g. `Eigen::MatrixXd` and
-    /// `Eigen::MatrixXf`), or its mapped version
-    /// (e.g. `Eigen::Map<Eigen::MatrixXd>`).
+    /// \param mat_ An **Eigen** sparse matrix object, whose type is
+    /// `Eigen::SparseMatrix<Scalar, ...>`.
     ///
-    DenseSymMatProd(ConstGenericMatrix& mat_) :
-        m_mat(mat_.data(), mat_.rows(), mat_.cols())
+    SparseSymMatProd(const SparseMatrix& mat_) :
+        m_mat(mat_)
     {}
 
     ///
@@ -73,4 +69,4 @@ public:
 
 } // namespace Spectra
 
-#endif // DENSE_SYM_MAT_PROD_H
+#endif // SPARSE_SYM_MAT_PROD_H
